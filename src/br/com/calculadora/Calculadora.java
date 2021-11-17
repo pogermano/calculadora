b3b1aba58a596aaca557149ceec359f059151fb6
pogermano <pogermano@gmail.com>
2021-11-17 12:41:53 -0300

    Adicionado output

--- src/br/com/calculadora/Calculadora.java
+++ src/br/com/calculadora/Calculadora.java
@@ -13,10 +13,11 @@ public class Calculadora {
 		b = scan.nextInt();
 		scan.close();
 
-		soma(a, b);
-		subtracao(a, b);
-		multiplicacao(a, b);
-		divisao(a, b);
+		System.out.println("O valor da soma dos números é : " + soma(a, b));
+		System.out.println("O valor da subtração dos números é : " + subtracao(a, b));
+		System.out.println("O valor da multiplicação dos números é : " + multiplicacao(a, b));
+		System.out.println("O valor da divisão dos números é : " + divisao(a, b));
+
 	}
 
 	private static int divisao(int a, int b) {
